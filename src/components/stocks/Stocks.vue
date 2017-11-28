858a368be7fc1426a1fc1d52e6fd5d45f854fd44
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-11-28 09:11:29 -0700

    More changes

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
-        <!--ToDo: Load app-stock component-->
+        <!--ToDo: Load app-stock component done -->
+        <app-stock></app-stock>
             <!--ToDo: Loop through stock in stocks using v-for-->
             <!--ToDo: Bind to stock using : and pass stock from the v-for-->
     </div>
 </template>
 
 <script>
-    //ToDo: Import Stock from ./Stock.vue
+    //ToDo: Import Stock from ./Stock.vue -- done
+    import Stock from './Stock.vue'
 
     export default {
-        //ToDo: Initialize Stock component and name it appStock
+        //ToDo: Initialize Stock component and name it appStock -- done
+        components: {
+            appStock: Stock
+        },
 
         computed: {
-            //ToDo: Create stocks computer method
-                // ToDo: Return $store.getters.stocks
+            //ToDo: Create stocks computer method -- done
+            stocks() {
+                return $store.getters.stocks
+            }
+                // ToDo: Return $store.getters.stocks -- done
         }
     }
 </script>
