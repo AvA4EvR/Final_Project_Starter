e2743da5eaa4d5edb2c7b8e4af6d8de5997f462b
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-11-28 09:04:06 -0700

    More changes

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import VueResource from 'vue-resource'
 //ToDo: Import App.vue and name it App -- done
 import App from './App.vue'
 
-//ToDo: Import routes.js and use the {} passing routes -- done?
+//ToDo: Import routes.js and use the {} passing routes -- done
 import {routes} from './routes.js'
 
 //ToDo: Import store/store.js and name it store -- done
-import {store} from './store/store.js';
+import store from './store/store';
 
 //ToDo Initialize VueRouter using Vue.use() -- done
 Vue.use(VueRouter);
@@ -22,19 +22,23 @@ Vue.use(VueResource);
 Vue.http.options.root = 'https://vuejs-http-f1e6b.firebaseio.com/data.json'; //ToDo: Set Equal to your Firebase Database URL -- done?
 
 Vue.filter('currency', (value) => {
-  //ToDo: Return value.toLocalString(), Add $ sign before the value -- done?
-    return $value.toLocaleString()
+  //ToDo: Return value.toLocalString(), Add $ sign before the value -- done
+    return '$' + value.toLocaleString()
 });
 
 const router = new VueRouter({
-    //ToDo: Set router mode to history -- done?
+    //ToDo: Set router mode to history -- done
     mode: 'history',
-    //ToDo: Pass routes constant imported from above
+    //ToDo: Pass routes constant imported from above -- done
+    routes
+
 });
 
 new Vue({
   el: '#app',
-    //ToDo: Pass the router constant to vue application
-    //ToDo: Pass store constant to vue application,
+    //ToDo: Pass the router constant to vue application -- done
+    router,
+    //ToDo: Pass store constant to vue application -- done
+    store,
   render: h => h(App)
 })
