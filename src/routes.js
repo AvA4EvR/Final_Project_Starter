4f3fe9fa36077a47ad43c26bcd2d610f3839e182
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-11-30 08:31:42 -0700

    Commit

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
-//ToDo: Import Home Component
-//ToDo: Import portfolio/Portfolio Component
-//ToDo: Import stocks/Stocks Component
+//ToDo: Import Home Component -- done
+//ToDo: Import portfolio/Portfolio Component -- done
+//ToDo: Import stocks/Stocks Component -- done
+import Home from './components/Home.vue'
+import Portfolio from './components/portfolio/Portfolio.vue'
+import Stocks from './components/stocks/Stocks.vue'
 
 export const routes = [
-    //ToDo: Create Route for Home Component with root (/) path
-    //ToDo: Create Route for Portfolio Component
-    //ToDo: Create Route for Stocks Component
+    //ToDo: Create Route for Home Component with root (/) path -- done
+    //ToDo: Create Route for Portfolio Component -- done
+    //ToDo: Create Route for Stocks Component -- done
+    { path: '/', name: 'home', components: {
+        default: Home
+    }},
+    { path: '/portfolio', name: 'portfolio', components: {
+        default: Portfolio
+    }},
+    { path: '/stocks', name: 'stocks', components: {
+        default: Stocks
+    }}
 ];
