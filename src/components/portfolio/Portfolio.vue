6ddfa9459e52a7cef4f280d35cef2e222501ac4e
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-12-05 08:02:39 -0700

    Portfolio.vue

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,21 +1,33 @@
 <template>
     <div>
-        <!--ToDo: Load app-stock component-->
-            <!--ToDo: Loop through stock in stocks using v-for-->
-            <!--ToDo: Bind to stock using : and pass stock from the v-for-->
+        <!--ToDo: Load app-stock component done-->
+            <!--ToDo: Loop through stock in stocks using v-for done-->
+            <!--ToDo: Bind to stock using : and pass stock from the v-for done-->
+        <app-stock>
+            <div v-for="stock in stocks" :stock="stock">
+            </div>
+        </app-stock>
     </div>
 </template>
 
 <script>
-    //ToDo: Import mapGetters from vuex
-    //ToDo: Import Stock from ./Stock.vue
+    //ToDo: Import mapGetters from vuex -- done
+    //ToDo: Import Stock from ./Stock.vue -- done
+    import { mapGetters } from 'vuex'
+    import Stock from './Stock.vue'
 
     export default {
         computed: {
-            //ToDo: Create ...mapGetters method
-                //ToDo: Call stocks: 'stockPortfolio'
+            //ToDo: Create ...mapGetters method -- done
+                //ToDo: Call stocks: 'stockPortfolio' -- done
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
         },
 
-        //ToDo: Initialize Stock component and name it appStock
+        //ToDo: Initialize Stock component and name it appStock -- done
+        components: {
+            appStock: Stock
+        }
     }
 </script>
